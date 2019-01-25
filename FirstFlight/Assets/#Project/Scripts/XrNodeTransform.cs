06d9eaf62cfd2ff77d2a7da222355b878b84c205
ThomasVanBouwel <van.bouwel.thomas@gmail.com>
2019-01-25 23:18:04 +0100

    simple user

---
+++ FirstFlight/Assets/#Project/Scripts/XrNodeTransform.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class XrNodeTransform : MonoBehaviour
+{
+    public XRNode hand;
+
+    private void Update()
+    {
+        transform.localPosition = InputTracking.GetLocalPosition(hand);
+        transform.localRotation = InputTracking.GetLocalRotation(hand);
+    }
+}
