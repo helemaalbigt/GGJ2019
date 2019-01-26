b4ec2e90b9436951ddfd5e7c80b8f02216f9dd70
ThomasVanBouwel <van.bouwel.thomas@gmail.com>
2019-01-26 10:35:01 +0100

    TileSpawn system

---
+++ FirstFlight/Assets/#Project/Scripts/GridLocater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class GridLocater : MonoBehaviour
+{
+    public event Action<Vector2> onCoordinatesChanged;
+
+    public Vector2 _coordinates;
+    public Transform _target;
+
+    private Vector2 _newCoords;
+
+    void Update()
+    {
+        _newCoords = PosToCoordinates(_target.position);
+
+        if(_newCoords != _coordinates)
+            FireNewCoordinates();
+
+        _coordinates = _newCoords;
+    }
+
+    private Vector2 PosToCoordinates(Vector3 pos)
+    {
+        return new Vector2(
+            (Mathf.RoundToInt(pos.x) + Math.Sign(pos.x) * (Globals.GridSize / 2)) / Globals.GridSize,
+            (Mathf.RoundToInt(pos.z) + Math.Sign(pos.z) * (Globals.GridSize / 2)) / Globals.GridSize
+        );
+    }
+
+    private void FireNewCoordinates()
+    {
+        if (onCoordinatesChanged != null)
+            onCoordinatesChanged(_newCoords);
+    }
+}
---
+++ FirstFlight/Assets/#Project/Scripts/TileSpawner.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+    public GameObject _tilePrefab;
+    public GridLocater _gridLocator;
+    public int[] _tileMargin = new int[4]{2, 1, 1, 1};
+
+    private int _arraySize;
+    private Transform[] _tilesInsitances;
+    private Vector2[] _occupiedTiles;
+
+    void Awake()
+    {
+        InitArrays();
+
+        _gridLocator.onCoordinatesChanged += UpdateTiles;
+        CoordsToOccupiedTiles(Vector2.zero);
+
+        SpawnTiles();
+        PositionTiles();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (_occupiedTiles == null || _occupiedTiles.Length == 0)
+            return;
+
+        Gizmos.color = Color.blue;
+        foreach (var coord in _occupiedTiles)
+        {
+            Gizmos.DrawWireCube(new Vector3((coord * Globals.GridSize).x, 0, (coord * Globals.GridSize).y), Vector3.one * Globals.GridSize);
+        }
+    }
+
+    private void UpdateTiles(Vector2 coords)
+    {
+        CoordsToOccupiedTiles(coords);
+        PositionTiles();
+    }
+
+    private void CoordsToOccupiedTiles(Vector2 coords)
+    {
+        int w = 0;
+
+        for (int i = -_tileMargin[3]; i <= _tileMargin[1]; i++)
+        {
+            for (int j = -_tileMargin[2]; j <= _tileMargin[0]; j++)
+            {
+                _occupiedTiles[w] = new Vector2(coords.x + i, coords.y + j);
+                w++;
+            }
+        }
+    }
+
+    private void InitArrays()
+    {
+        _arraySize = (_tileMargin[0] + _tileMargin[2] + 1) * (_tileMargin[1] + _tileMargin[3] + 1);
+        _tilesInsitances = new Transform[_arraySize];
+        _occupiedTiles = new Vector2[_arraySize];
+    }
+
+    private void SpawnTiles()
+    {
+        for (int i = 0; i < _arraySize; i++)
+        {
+            var go = Instantiate(_tilePrefab);
+            _tilesInsitances[i] = go.transform;
+        }
+    }
+
+    private void PositionTiles()
+    {
+        for (int i = 0; i < _arraySize; i++)
+        {
+            _tilesInsitances[i].position = new Vector3(
+                _occupiedTiles[i].x * Globals.GridSize,
+                0,
+                _occupiedTiles[i].y * Globals.GridSize
+                );
+        }
+    }
+}
