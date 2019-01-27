b9f1c5419d277e6c5bad5275f9a69f604a36a2b5
ThomasVanBouwel <van.bouwel.thomas@gmail.com>
2019-01-27 13:40:27 +0100

    build version

--- FirstFlight/Assets/#Project/Scripts/TileSpawner.cs
+++ FirstFlight/Assets/#Project/Scripts/TileSpawner.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class TileSpawner : MonoBehaviour
 {
     public GameObject _tilePrefab;
+    public GameObject _lowPolyTile;
     public GridLocater _gridLocator;
     public int[] _tileMargin = new int[4]{2, 1, 1, 1};
+    public int[] _lowPolyTileMargin = new int[4] { 3, 3, 3, 1 };
 
     private int _arraySize;
+    private int _lowPolyArraySize;
     private Transform[] _tilesInsitances;
     private Vector2[] _occupiedTiles;
+
+    private Transform[] _lowPolyTileInstances;
+    private Vector2[] _lowPolyOccupiedTiles;
 
     void Awake()
     {
@@ -30,6 +36,12 @@ public class TileSpawner : MonoBehaviour
 
         Gizmos.color = Color.blue;
         foreach (var coord in _occupiedTiles)
+        {
+            Gizmos.DrawWireCube(new Vector3((coord * Globals.GridSize).x, 0, (coord * Globals.GridSize).y), Vector3.one * Globals.GridSize);
+        }
+
+        Gizmos.color = Color.yellow;
+        foreach (var coord in _lowPolyOccupiedTiles)
         {
             Gizmos.DrawWireCube(new Vector3((coord * Globals.GridSize).x, 0, (coord * Globals.GridSize).y), Vector3.one * Globals.GridSize);
         }
@@ -60,6 +72,10 @@ public class TileSpawner : MonoBehaviour
         _arraySize = (_tileMargin[0] + _tileMargin[2] + 1) * (_tileMargin[1] + _tileMargin[3] + 1);
         _tilesInsitances = new Transform[_arraySize];
         _occupiedTiles = new Vector2[_arraySize];
+
+        _lowPolyArraySize = ((_lowPolyTileMargin[0] + _lowPolyTileMargin[2] + 1) * (_lowPolyTileMargin[1] + _lowPolyTileMargin[3] + 1)) - _arraySize;
+        _lowPolyTileInstances = new Transform[_lowPolyArraySize];
+        _lowPolyOccupiedTiles = new Vector2[_lowPolyArraySize];
     }
 
     private void SpawnTiles()
